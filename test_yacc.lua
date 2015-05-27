8ec3713da778f69cc28178dd86332765d2706f9f
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-05-27 13:02:40 +0900

    yacc互換のテストを修正。

--- test_yacc.lua
+++ test_yacc.lua
@@ -16,18 +16,91 @@
 -- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
 
 local regexp = require "dromozoa.regexp"
-local match = require "dromozoa.regexp.match"
+local scan = require "dromozoa.regexp.scan"
+local scanner = require "dromozoa.regexp.scanner"
+local dump = require "dromozoa.regexp.dump"
+local identity_generator = require "dromozoa.parser.identity_generator"
 
-local re_comment = regexp([[/\*]]):concat(regexp([[.*]]):difference([[.*\*/.*]])):concat([[\*/]])
-local re_name = regexp("[[:alpha:]._][[:alnum:]._]*")
-local re_literal = regexp([=['([^'\]|\\(['"?\abfnrtv]|[0-7]{1,3}|x[[:xdigit:]]+))']=])
-local re_number = regexp("[[:digit:]]+")
+local tokens = identity_generator()
 
-re_comment:write_graphviz(assert(io.open("test-re_comment.dot", "w"))):close()
-re_name:write_graphviz(assert(io.open("test-re_name.dot", "w"))):close()
-re_literal:write_graphviz(assert(io.open("test-re_literal.dot", "w"))):close()
-re_number:write_graphviz(assert(io.open("test-re_number.dot", "w"))):close()
+local re = regexp([[/\*]])
+  :concat(regexp([[.*]]):difference([[.*\*/.*]]))
+  :concat([[\*/]])
+  :set_token(tokens.TK_COMMENT)
+  :branch("[[:space:]]+", tokens.TK_WHITESPACE)
+  :branch([[[[:alpha:]._][[:alnum:]._]*]], tokens.TK_NAME)
+  :branch([['\\'']], tokens["TK_ESCAPE_SEQUENCE(')"])
+  :branch([['\\"']], tokens["TK_ESCAPE_SEQUENCE(\")"])
+  :branch([['\\\?']], tokens["TK_ESCAPE_SEQUENCE(?)"])
+  :branch([['\\\\']], tokens["TK_ESCAPE_SEQUENCE(\\)"])
+  :branch([['\\a']], tokens["TK_ESCAPE_SEQUENCE(a)"])
+  :branch([['\\b']], tokens["TK_ESCAPE_SEQUENCE(b)"])
+  :branch([['\\f']], tokens["TK_ESCAPE_SEQUENCE(f)"])
+  :branch([['\\n']], tokens["TK_ESCAPE_SEQUENCE(n)"])
+  :branch([['\\r']], tokens["TK_ESCAPE_SEQUENCE(r)"])
+  :branch([['\\t']], tokens["TK_ESCAPE_SEQUENCE(t)"])
+  :branch([['\\v']], tokens["TK_ESCAPE_SEQUENCE(v)"])
+  :branch([['\\[0-7]{1,3}']], tokens.TK_ESCAPE_OCT)
+  :branch([=['\\x[[:xdigit:]]+']=], tokens.TK_ESCAPE_OCT)
+  :branch("'[^'\\\n]'", tokens.TK_LITERAL)
+  :branch("[[:digit:]]+", tokens.TK_NUMBER)
+  :branch("%token", tokens.TK_TOKEN)
+  :branch("%left", tokens.TK_LEFT)
+  :branch("%right", tokens.TK_RIGHT)
+  :branch("%nonassoc", tokens.TK_NONASSOC)
+  :branch("%type", tokens.TK_TYPE)
+  :branch("%start", tokens.TK_START)
+  :branch("%%", tokens.TK_MARK)
+  :branch(regexp("%\\{"):concat(regexp(".*"):difference(".*%}.*")):concat("%}"):set_token(tokens.TK_CODE))
+  :branch("\\{", tokens.TK_ACTION_BEGIN)
+  :branch(":", tokens["TK(:)"])
+  :branch(";", tokens["TK(;)"])
+  :branch("\\|", tokens["TK(|)"])
 
-print(arg[1])
-print(match(re_literal:compile(), arg[1]))
+local re_action = regexp("}", tokens.TK_ACTION_END)
+  :branch([[\$\$]], tokens["TK_ACTION($)"])
+  :branch("\\{", tokens.TK_ACTION_BEGIN)
+  :branch("[^{}$]+", tokens.TK_ACTION_TEXT)
+
+-- re:write_graphviz(assert(io.open("test-re.dot", "w"))):close()
+-- re_action:write_graphviz(assert(io.open("test-re_action.dot", "w"))):close()
+
+-- dump(re:compile(), io.stdout)
+-- dump(re_action:compile(), io.stderr)
+
+-- os.exit()
+
+local actions = {}
+for i = 1, #tokens do
+  actions[i] = scanner.PUSH
+end
+actions[tokens.TK_COMMENT] = scanner.SKIP
+actions[tokens.TK_WHITESPACE] = scanner.SKIP
+actions[tokens.TK_ACTION_BEGIN] = scanner.CALL(2)
+actions[tokens.TK_ACTION_END] = scanner.RETURN
+
+
+local s = io.read("*a")
+local stack, begins, ends = scan({ re:compile(), re_action:compile() }, actions, s)
+for i = 1, #stack do
+  print(string.format("%d\t%q", stack[i], s:sub(begins[i], ends[i])))
+end
+
+
+
+
+
+-- local re = regexp(
+-- 
+-- local re_name = regexp("[[:alpha:]._][[:alnum:]._]*")
+-- local re_literal = regexp([=['([^'\]|\\(['"?\abfnrtv]|[0-7]{1,3}|x[[:xdigit:]]+))']=])
+-- local re_number = regexp("[[:digit:]]+")
+-- 
+-- re_comment:write_graphviz(assert(io.open("test-re_comment.dot", "w"))):close()
+-- re_name:write_graphviz(assert(io.open("test-re_name.dot", "w"))):close()
+-- re_literal:write_graphviz(assert(io.open("test-re_literal.dot", "w"))):close()
+-- re_number:write_graphviz(assert(io.open("test-re_number.dot", "w"))):close()
+-- 
+-- print(arg[1])
+-- print(match(re_literal:compile(), arg[1]))
 
