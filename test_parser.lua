68f8476b2f59a5b50f5884775d4fdee6f875ab0e
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-06-16 15:56:49 +0900

    解析器の実装を修正。

--- test_parser.lua
+++ test_parser.lua
@@ -1,73 +1,125 @@
-local identity_generator = require "dromozoa.parser.identity_generator"
+local json = require "dromozoa.json"
 
-local function construct_grammar(_symbols, _grammar)
-  if not _symbols then
-    _symbols = identity_generator()
+local function equal(a, b)
+  if a == b then
+    return true
+  else
+    if type(a) == "table" and type(b) == "table" then
+      for k, u in next, a do
+        local v = b[k]
+        if v == nil or not equal(u, v) then
+          return false
+        end
+      end
+      for k in next, b do
+        if a[k] == nil then
+          return false
+        end
+      end
+      return getmetatable(a) == getmetatable(b)
+    else
+      return false
+    end
   end
+end
+
+local function find(set, value)
+  for i = 1, #set do
+    if equal(set[i], value) then
+      return i
+    end
+  end
+end
+
+local function set_insert(set, value)
+  if not find(set, value) then
+    set[#set + 1] = value
+  end
+end
+
+local function set_merge(a, b)
+  local set = {}
+  for i = 1, #a do
+    set_insert(set, a[i])
+  end
+  for i = 1, #b do
+    set_insert(set, b[i])
+  end
+  return set
+end
+
+local function map_keyset(map)
+  local set = {}
+  for k in pairs(map) do
+    set[#set + 1] = k
+  end
+  return set
+end
+
+local function map_insert(map, key, value)
+  local set = map[key]
+  if set then
+    set_insert(set, value)
+  else
+    map[key] = { value }
+  end
+end
+
+local function tokenize(text)
+  local tokens = {}
+  for token in text:gmatch("[^%s]+") do
+    tokens[#tokens + 1] = token
+  end
+  return tokens
+end
+
+local function construct_grammar(_grammar)
   if not _grammar then
     _grammar = {}
   end
 
   local self = {}
 
-  local function parse_rule(line)
-    local head, body = line:match("^%s*(.-)%s*%->(.*)")
-    if head then
-      local dot
-      local rule = {
-        head = _symbols[head];
-        body = {}
-      }
-      for v in body:gmatch("[^%s]+") do
-        if v == "." then
-          rule.dot = #rule.body + 1
-        else
-          table.insert(rule.body, _symbols[v])
-        end
-      end
-      return rule
-    end
-  end
-
   function self:parse(text)
     for line in text:gmatch("[^\n]+") do
       if not line:match("^%s*#") then
-        local rule = parse_rule(line)
-        if rule then
-          table.insert(_grammar, rule)
+        local tokens = tokenize(line)
+        local head = table.remove(tokens, 1)
+        if table.remove(tokens, 1) == "->" then
+          map_insert(_grammar, head, tokens)
         end
       end
     end
     return self
   end
 
-  local function unparse_rule(rule)
-    local buffer = { _symbols[rule.head], "->" }
-    for i, v in ipairs(rule.body) do
-      if rule.dot == i then
-        table.insert(buffer, ".")
-      end
-      table.insert(buffer, _symbols[v])
-    end
-    if rule.dot == #rule.body + 1 then
-      table.insert(buffer, ".")
-    end
-    return table.concat(buffer, " ")
-  end
-
   function self:unparse(out)
-    for _, rule in ipairs(_grammar) do
-      out:write(unparse_rule(rule), "\n")
-    end
-  end
-
-  local function is_terminal_symbol(symbol)
-    for _, rule in ipairs(_grammar) do
-      if rule.head == symbol then
-        return false
+    for head, bodies in pairs(_grammar) do
+      for _, body in ipairs(bodies) do
+        out:write(head, " ->")
+        for _, v in ipairs(body) do
+          out:write(" ", v)
+        end
+        out:write("\n")
       end
     end
-    return true
+    return out
+  end
+
+  function self:is_terminal_symbol(symbol)
+    return not _grammar[symbol]
+  end
+
+  function eliminate_left_recursion()
+    local nonterminal_symbols = map_keyset(_grammar)
+    for i = 1, #nonterminal_symbols do
+      local symbol_i = nonterminal_symbols[i]
+      local bodies_i = _grammar[symbol_i]
+      for j = 1, i - 1 do
+        local symbol_j = nonterminal_symbols[j]
+        local bodies_j = _grammar[symbol_j]
+      end
+    end
   end
 
   return self
@@ -83,3 +135,11 @@ F -> id
 ]])
 
 grammar:unparse(io.stdout)
+
+print(grammar:is_terminal_symbol("id"))
+print(grammar:is_terminal_symbol("E"))
+
+-- for head, rules in pairs(grammar:rules()) do
+--   print(head, #rules)
+-- end
+
