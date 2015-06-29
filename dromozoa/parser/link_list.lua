60a6e0f50d1b560f11a6a8e10aff01ccb43101d0
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-06-29 10:24:35 +0900

    link_listを更新。

--- dromozoa/parser/link_list.lua
+++ dromozoa/parser/link_list.lua
@@ -15,59 +15,63 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
 
--- p, t, n
-
-local function insert(prev, value)
-  local next = prev[2]
-  local this = { value, next, prev }
-  prev[2] = this
-  next[3] = this
-end
-
-local function remove(this)
-  local next = this[2]
-  local prev = this[3]
-  prev[2] = next
-  next[3] = prev
-end
-
 local function construct()
-  local head = {}
-  local tail = {}
+  local head = 0
+  local tail = 0
 
-  head[2] = tail
-  tail[3] = head
+  local _prev = { [tail] = head }
+  local _next = { [head] = tail }
+  local _value = {}
+  local _n = 0
 
   local this = {}
 
+  function this:insert(prev, value)
+    local next = _next[prev]
+    local id = _n + 1
+    _prev[id] = prev
+    _next[id] = next
+    _value[id] = value
+    _n = id
+    _prev[next] = id
+    _next[prev] = id
+    return id
+  end
+
+  function this:remove(id)
+    local prev = _prev[id]
+    local next = _next[id]
+    local value = _value[id]
+    _prev[id] = nil
+    _next[id] = nil
+    _value[id] = nil
+    _next[prev] = next
+    _prev[next] = prev
+    return value
+  end
+
   function this:push_front(value)
-    insert(head, value)
+    return this:insert(head, value)
   end
 
   function this:push_back(value)
-    insert(tail[3], value)
+    return this:insert(_prev[tail], value)
   end
 
   function this:pop_front()
-    local node = head[2]
-    local v = node[1]
-    remove(node)
-    return v
+    return this:remove(_next[head])
   end
 
   function this:pop_back()
-    local node = tail[3]
-    local v = node[1]
-    remove(node)
-    return v
+    return this:remove(_prev[tail])
   end
 
   function this:each()
     return coroutine.wrap(function ()
-      local this = head[2]
-      while this ~= tail do
-        coroutine.yield(this[1])
-        this = this[2]
+      local i = _next[head]
+      while i ~= tail do
+        coroutine.yield(_value[i])
+        i = _next[i]
       end
     end)
   end
@@ -75,4 +79,6 @@ local function construct()
   return this
 end
 
-return construct
+return function ()
+  return construct({}, {}, {}, 0)
+end
--- test_link_list.lua
+++ test_link_list.lua
@@ -2,11 +2,18 @@ local link_list = require "dromozoa.parser.link_list"
 
 local list = link_list()
 
-list:push_front(1)
-list:push_front(2)
-list:push_front(3)
+print(list:push_back(4))
+print(list:push_back(5))
+print(list:push_back(6))
+print(list:push_front(1))
+print(list:push_front(2))
+print(list:push_front(3))
+print("--")
 print(list:pop_front())
+print(list:pop_back())
+print("--")
 
 for v in list:each() do
   print(v)
 end
+
