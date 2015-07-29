09ff813c7d96f9c2e0d64bf9cacc2e1fd5a67336
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-07-29 15:17:49 +0900

    LALRの実装を開始。

--- test_parser4.lua
+++ test_parser4.lua
@@ -389,6 +389,29 @@ local function lr0_kernels(rules, start_item)
   return set_of_kernel_items
 end
 
+local function determine_lookaheads(rules, start_item, symbol)
+  local set_of_kernel_items = lr0_kernels(rules, start_item)
+  local map = linked_hash_table()
+
+  for kernel_items in set_of_kernel_items:each() do
+    for kernel_item in kernel_items:each() do
+      unparse_item(kernel_item, io.stdout)
+      local head, body, dot = kernel_item[1], kernel_item[2], kernel_item[3]
+      local items = lr1_closure(rules, sequence():push(make_item(head, body, dot, "#")))
+      for item in items:each() do
+        io.write("  ")
+        unparse_item(item, io.stdout)
+        local term = item[4]
+        if term == "#" then
+          -- conclude that lookaheads propagate from `A -> a dot b` in I to `B -> c X dot d` in GOTO(I, X)
+        else
+          -- conclude that lookahead a is generated spontaneously for item `B -> c X dot d` in GOTO(I, X)
+        end
+      end
+    end
+  end
+end
+
 local rules = parse_grammar([[
 S -> A a
 S -> b
@@ -495,3 +518,6 @@ for items in set_of_kernel_items:each() do
     unparse_item(item, io.stdout)
   end
 end
+
+io.write("==\n")
+determine_lookaheads(rules, make_item("S'", { "S" }, 1))
