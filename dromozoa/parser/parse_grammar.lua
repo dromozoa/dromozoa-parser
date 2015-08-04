498aca92cb99527ea22f510b30849e45f61c52bb
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-08-04 11:27:09 +0900

    parse_grammarを追加。

---
+++ dromozoa/parser/parse_grammar.lua
@@ -0,0 +1,45 @@
+-- Copyright (C) 2015 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-parser.
+--
+-- dromozoa-parser is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-parser is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
+
+local linked_hash_table = require "dromozoa.commons.linked_hash_table"
+local sequence = require "dromozoa.commons.sequence"
+
+return function (text)
+  local rules = sequence()
+  local names = linked_hash_table()
+  for line in text:gmatch("[^\n]+") do
+    if not line:match("^%s*#") then
+      local head
+      local body = sequence()
+      for symbol in line:gmatch("%S+") do
+        if symbol == "->" then
+          assert(head ~= nil)
+          assert(#body == 0)
+        else
+          names:insert(symbol)
+          if head == nil then
+            head = names:identity(symbol)
+          else
+            body:push(names:identity(symbol))
+          end
+        end
+      end
+      rules:push(sequence():push(head, body))
+    end
+  end
+  return names, rules
+end
---
+++ test_parser2.lua
@@ -0,0 +1,20 @@
+local parse_grammar = require "dromozoa.parser.parse_grammar"
+local json = require "dromozoa.json"
+
+local names, rules = parse_grammar([[
+S' -> S
+S -> L = R
+S -> R
+L -> * R
+L -> id
+R -> L
+]])
+
+for rule in rules:each() do
+  local head, body = rule[1], rule[2]
+  io.write(names:find(head), " ->")
+  for symbol in body:each() do
+    io.write(" ", (names:find(symbol)))
+  end
+  io.write("\n")
+end
