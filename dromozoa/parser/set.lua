185539ba95697ddbee38b86e4e75b8c1afa58385
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-08-10 14:40:33 +0900

    集合演算を追加。

---
+++ dromozoa/parser/set.lua
@@ -0,0 +1,33 @@
+-- Copyright (C) 2015 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-parser.
+--
+-- dromozoa-parser is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-parser is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
+
+local pairs = require "dromozoa.commons.pairs"
+
+local class = {}
+
+function class.set_union(a, b)
+  local n = 0
+  for k, v in pairs(b) do
+    if a[k] == nil then
+      n = n + 1
+      a[k] = v
+    end
+  end
+  return n
+end
+
+return class
