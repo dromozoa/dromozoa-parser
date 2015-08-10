3d3011c80741b32d4c84465673c831c7813715f7
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-08-11 08:51:44 +0900

    左再帰の除去を追加。

--- test_parser2.lua
+++ test_parser2.lua
@@ -1,20 +1,35 @@
-local parse = require "dromozoa.parser.parse"
-local json = require "dromozoa.json"
+local clone = require "dromozoa.commons.clone"
+local parse_grammar = require "dromozoa.parser.parse_grammar"
+local eliminate_left_recursion = require "dromozoa.parser.eliminate_left_recursion"
 
-local prods = parse([[
-S' -> S
-S -> L = R
-S -> R
-L -> * R
-L -> id
-R -> L
+local prods = parse_grammar([[
+S -> A a
+S -> b
+A -> A c
+A -> S d
+A ->
 ]])
 
-for head, bodies in prods:each() do
+local prods2 = clone(prods)
+eliminate_left_recursion(prods2)
+
+local function write_symbol(sym)
+  if type(sym) == "table" then
+    for i = 1, #sym do
+      write_symbol(sym[i])
+    end
+  else
+    io.write(sym)
+  end
+end
+
+for head, bodies in prods2:each() do
   for body in bodies:each() do
-    io.write(head, " ->")
+    write_symbol(head)
+    io.write(" ->")
     for sym in body:each() do
-      io.write(" ", sym)
+      io.write(" ")
+      write_symbol(sym)
     end
     io.write("\n")
   end
