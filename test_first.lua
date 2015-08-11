ce9dcf80c332d57dccf6670118cc5d8561bc74e0
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-08-11 09:18:46 +0900

    テストを追加。

---
+++ test_first.lua
@@ -0,0 +1,73 @@
+-- Copyright (C) 2015 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-parser.
+--
+-- dromozoa-parser is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-parser is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
+
+local equal = require "dromozoa.commons.equal"
+local linked_hash_table = require "dromozoa.commons.linked_hash_table"
+local sequence = require "dromozoa.commons.sequence"
+local json = require "dromozoa.json"
+local parse_grammar = require "dromozoa.parser.parse_grammar"
+local eliminate_left_recursion = require "dromozoa.parser.eliminate_left_recursion"
+local first = require "dromozoa.parser.first"
+
+local prods = parse_grammar([[
+E -> E + T
+E -> T
+T -> T * F
+T -> F
+F -> ( E )
+F -> id
+]])
+
+eliminate_left_recursion(prods)
+
+local expected = linked_hash_table()
+expected["E"] = {
+  { "T", { "E", "'" } };
+}
+expected[{ "E", "'" }] = {
+  { "+", "T", { "E", "'" } };
+  {};
+}
+expected["T"] = {
+  { "F", { "T", "'" } };
+}
+expected[{ "T", "'" }] = {
+  { "*", "F", { "T", "'" } };
+  {};
+}
+expected["F"] = {
+  { "(", "E", ")" };
+  { "id" };
+}
+assert(equal(prods, expected))
+
+local expected = linked_hash_table()
+expected:insert("(")
+expected:insert("id")
+assert(equal(first(prods, sequence({ "F" })), expected))
+assert(equal(first(prods, sequence({ "T" })), expected))
+assert(equal(first(prods, sequence({ "E" })), expected))
+
+local expected = linked_hash_table()
+expected:insert("+")
+expected:insert({})
+assert(equal(first(prods, sequence({ { "E", "'" } })), expected))
+
+local expected = linked_hash_table()
+expected:insert("*")
+expected:insert({})
+assert(equal(first(prods, sequence({ { "T", "'" } })), expected))
