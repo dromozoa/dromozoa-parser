7e7a588e332f28028a5cb376aa3a8fcbac3c321d
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-08-13 13:24:46 +0900

    sequence_writerを追加。

--- test_parser2.lua
+++ test_parser2.lua
@@ -1,5 +1,6 @@
 local clone = require "dromozoa.commons.clone"
 local eliminate_left_recursion = require "dromozoa.parser.eliminate_left_recursion"
+local sequence_writer = require "dromozoa.parser.sequence_writer"
 local test = require "test"
 
 local prods = test.parse_grammar([[
@@ -12,4 +13,6 @@ A ->
 
 local prods2 = clone(prods)
 eliminate_left_recursion(prods2)
-test.unparse_grammar(io.stdout, prods)
+local result = test.unparse_grammar(sequence_writer(), prods2):concat()
+
+io.write(result)
