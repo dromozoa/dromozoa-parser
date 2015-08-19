79675da1a2b0587c7d602a5f748acecc1f927b04
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-08-19 17:50:02 +0900

    ファイルを分割。

--- dromozoa/parser/lr1.lua
+++ dromozoa/parser/lr1.lua
@@ -15,43 +15,14 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
 
-local clone = require "dromozoa.commons.clone"
 local equal = require "dromozoa.commons.equal"
-local linked_hash_table = require "dromozoa.commons.linked_hash_table"
 local sequence = require "dromozoa.commons.sequence"
-local first_symbols = require "dromozoa.parser.first_symbols"
 local items = require "dromozoa.parser.items"
+local lr1_closure = require "dromozoa.parser.lr1_closure"
 
-local class = {}
-
-function class.closure(prods, items)
-  local items = clone(items)
-  local added = linked_hash_table()
-  local done
-  repeat
-    done = true
-    for item in items:each() do
-      local head, body, dot, term = item[1], item[2], item[3], item[4]
-      local symbol = body[dot]
-      if symbol ~= nil then
-        local bodies = prods[symbol]
-        if bodies ~= nil then
-          local first = first_symbols(prods, sequence():copy(body, dot + 1):push(term))
-          for body in bodies:each() do
-            for term in first:each() do
-              local item = sequence():push(symbol, body, 1, term)
-              if added:insert(item) == nil then
-                items:push(item)
-                done = false
-              end
-            end
-          end
-        end
-      end
-    end
-  until done
-  return items
-end
+local class = {
+  closure = lr1_closure;
+}
 
 function class.goto_(prods, items, symbol)
   local goto_items = sequence()
