c9be10373aeade0e5e5ac54df4ef5f38c60c6911
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-08-19 18:00:19 +0900

    ファイル名を変更。

--- dromozoa/parser/lr0.lua
+++ dromozoa/parser/lr0.lua
@@ -18,9 +18,9 @@
 local clone = require "dromozoa.commons.clone"
 local linked_hash_table = require "dromozoa.commons.linked_hash_table"
 local sequence = require "dromozoa.commons.sequence"
-local lr_impl = require "dromozoa.parser.lr_impl"
+local lr = require "dromozoa.parser.lr"
 
-return lr_impl(function (prods, items)
+return lr(function (prods, items)
   local items = clone(items)
   local added = linked_hash_table()
   local done
