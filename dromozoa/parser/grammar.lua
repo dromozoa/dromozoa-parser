c7f05b4ba1f2b495f67d3983e2543b5ee7c47277
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-11-21 10:43:13 +0900

    grammarを修正。

--- dromozoa/parser/grammar.lua
+++ dromozoa/parser/grammar.lua
@@ -15,12 +15,39 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
 
+local apply = require "dromozoa.commons.apply"
+local linked_hash_table = require "dromozoa.commons.linked_hash_table"
+
 local class = {}
 
-function class.new(prods)
+function class.new(prods, start)
+  if start == nil then
+    start = apply(prods:each())
+  end
   return {
     prods = prods;
+    start = start;
   }
+end
+
+function class:each_symbol()
+  local prods = self.prods
+  return coroutine.wrap(function ()
+    local map = linked_hash_table()
+    for head in prods:each() do
+      map:insert(head)
+      coroutine.yield(head, false)
+    end
+    for head, bodies in prods:each() do
+      for body in bodies:each() do
+        for symbol in body:each() do
+          if map:insert(symbol) == nil then
+            coroutine.yield(symbol, true)
+          end
+        end
+      end
+    end
+  end)
 end
 
 local metatable = {
@@ -28,7 +55,7 @@ local metatable = {
 }
 
 return setmetatable(class, {
-  __call = function (_, prods)
-    return setmetatable(class.new(prods), metatable)
+  __call = function (_, prods, start)
+    return setmetatable(class.new(prods, start), metatable)
   end;
 })
---
+++ test/test_grammar.lua
@@ -0,0 +1,53 @@
+-- Copyright (C) 2015 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-parser.
+--
+-- dromozoa-parser is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-parser is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
+
+local equal = require "dromozoa.commons.equal"
+local linked_hash_table = require "dromozoa.commons.linked_hash_table"
+local syntax_tree = require "dromozoa.parser.syntax_tree"
+
+local ast = syntax_tree()
+
+local B = ast:builder()
+B.E = B.E * B["+"] * B.T
+    + B.T
+B.T = B.T * B["*"] * B.F
+    + B.F
+B.F = B["("] * B.E * B[")"]
+    + B.id
+
+ast:write_graphviz(assert(io.open("test1.dot", "w"))):close()
+local grammar = ast:to_grammar()
+ast:write_graphviz(assert(io.open("test2.dot", "w"))):close()
+
+assert(grammar.start == "E")
+grammar.start = "T"
+assert(grammar.start == "T")
+
+local symbols = linked_hash_table()
+for symbol, is_terminal_symbol in grammar:each_symbol() do
+  assert(symbols:insert(symbol, is_terminal_symbol) == nil)
+end
+assert(equal(symbols, {
+  E = false;
+  T = false;
+  F = false;
+  ["+"] = true;
+  ["*"] = true;
+  ["("] = true;
+  [")"] = true;
+  id = true;
+}))
