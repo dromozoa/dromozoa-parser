c52bc8c631956518fe9fb92d13c45abc8a614d46
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-05-02 08:59:59 +0900

    テストを修正。

--- test/test_lr0.lua
+++ test/test_lr0.lua
@@ -16,71 +16,9 @@
 -- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
 
 local dumper = require "dromozoa.commons.dumper"
-local ipairs = require "dromozoa.commons.ipairs"
 local sequence = require "dromozoa.commons.sequence"
-local sequence_writer = require "dromozoa.commons.sequence_writer"
-local xml = require "dromozoa.commons.xml"
 local grammar = require "dromozoa.parser.builder.grammar"
-
-local TO = string.char(0xE2, 0x86, 0x92) -- U+2192 RIGHWARDS ARROW
-local DOT = string.char(0xC2, 0xB7) -- U+00B7 MIDDLE DOT
-
-local function dump_item(out, g, item)
-  local productions = g.productions
-  local symbols = g.symbols
-  local production = productions[item.id]
-  local body = production.body
-  local dot = item.dot
-  out:write(symbols[production.head], " ", TO)
-  for i, symbol in ipairs(body) do
-    if i == dot then
-      out:write(" ", DOT)
-    end
-    out:write(" ", symbols[symbol])
-  end
-  if dot == #body + 1 then
-    out:write(" ", DOT)
-  end
-  return out
-end
-
-local function dump_items(g, items)
-  for item in items:each() do
-    dump_item(io.stdout, g, item)
-    io.write("\n")
-  end
-end
-
-local function write_graph(g, set_of_items, transitions, filename)
-  local symbols = g.symbols
-
-  local out = assert(io.open(filename, "w"))
-  out:write([[
-digraph g {
-graph [rankdir=LR];
-node [shape=plaintext]
-]])
-
-  for i, items in ipairs(set_of_items) do
-    out:write(("%d [label=<<table border=\"1\" cellborder=\"0\" cellpadding=\"0\" cellspacing=\"0\" margin=\"0\">"):format(i))
-    out:write(("<tr><td>I%d</td></tr>"):format(i))
-    for item in items:each() do
-      out:write(("<tr><td align=\"left\" bgcolor=\"%s\">%s</td></tr>"):format(
-          g:is_kernel_item(item) and "white" or "grey",
-          xml.escape(dump_item(sequence_writer(), g, item):concat())))
-    end
-    out:write("</table>>]\n")
-  end
-
-  for transition, to in transitions:each() do
-    out:write(([[
-%d->%d [label=<%s>];
-]]):format(transition.from, to, xml.escape(symbols[transition.symbol])))
-  end
-
-  out:write("}\n")
-  out:close()
-end
+local dump = require "test.dump"
 
 local _ = grammar()
 
@@ -96,7 +34,7 @@ local g = _():argument()
 local I = sequence():push():push({ id = 7, dot = 1 })
 -- print("--")
 -- print(dumper.encode(I, { pretty = true }))
--- dump_items(g, I)
+-- dump.items(io.stdout, g, I)
 
 -- E' -> dot E
 -- E -> dot E + T
@@ -108,7 +46,7 @@ local I = sequence():push():push({ id = 7, dot = 1 })
 g:lr0_closure(I)
 -- print("--")
 -- print(dumper.encode(I, { pretty = true }))
--- dump_items(g, I)
+-- dump.items(io.stdout, g, I)
 
 -- E' -> E dot
 -- E -> E dot + T
@@ -116,7 +54,7 @@ local I = sequence()
   :push({ id = 7, dot = 2 })
   :push({ id = 1, dot = 2 })
 -- print("--")
--- dump_items(g, I)
+-- dump.items(io.stdout, g, I)
 
 -- E -> E + dot T
 -- T -> dot T * F
@@ -125,12 +63,8 @@ local I = sequence()
 -- F -> dot id
 local J = g:lr0_goto(I)[1]
 -- print("--")
--- dump_items(g, J)
+-- dump.items(io.stdout, g, J)
 
 local set_of_items, transitions = g:lr0_items()
-for i, items in ipairs(set_of_items) do
-  io.write(("======== I_%d ==========\n"):format(i))
-  dump_items(g, items)
-end
-
-write_graph(g, set_of_items, transitions, "test.dot")
+dump.set_of_items(io.stdout, g, set_of_items)
+dump.write_graph("test.dot", g, set_of_items, transitions)
