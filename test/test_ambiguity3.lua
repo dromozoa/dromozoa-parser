71ad35a87482613310f916a5b4305633e5d750a7
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-05-11 17:32:43 +0900

    テストを追加。

---
+++ test/test_ambiguity3.lua
@@ -0,0 +1,73 @@
+-- Copyright (C) 2017 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-parser.
+--
+-- dromozoa-parser is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-parser is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
+
+local dumper = require "dromozoa.commons.dumper"
+local builder = require "dromozoa.parser.builder"
+local driver = require "dromozoa.parser.driver"
+
+local _ = builder()
+
+_ :pat "%s+" :ignore ()
+  :lit "=="
+  :lit "!="
+  :lit "<"
+  :lit "<="
+  :lit ">"
+  :lit ">="
+  :lit "id"
+
+_ :nonassoc "==" "!="
+  :nonassoc "<" "<=" ">" ">="
+
+_ "E"
+  :_ "E" "==" "E"
+  :_ "E" "!=" "E"
+  :_ "E" "<" "E"
+  :_ "E" "<=" "E"
+  :_ "E" ">" "E"
+  :_ "E" ">=" "E"
+  :_ "id"
+
+local scanner, grammar, writer = _:build()
+
+local set_of_items, transitions = grammar:lalr1_items()
+
+writer:write_set_of_items(io.stdout, set_of_items)
+writer:write_graph(assert(io.open("test-graph.dot", "w")), transitions):close()
+
+local data = grammar:lr1_construct_table(set_of_items, transitions, io.stdout)
+writer:write_table(assert(io.open("test.html", "w")), data):close()
+local driver = driver(data)
+
+local source = [[
+id < id < id
+]]
+
+local position = 1
+while true do
+  local symbol, i, j = assert(scanner(source, position))
+  print(symbol, writer.symbol_names[symbol], i, j, source:sub(i, j))
+  if symbol == 1 then
+    assert(driver:parse())
+    break
+  else
+    assert(driver:parse(symbol, { value = source:sub(i, j) }))
+  end
+  position = j + 1
+end
+
+writer:write_tree(assert(io.open("test-tree.dot", "w")), driver.tree):close()
