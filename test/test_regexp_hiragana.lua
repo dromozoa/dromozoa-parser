5cf9b7450e99c66cdad701ef4a5762a4a75aff64
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-07-26 09:25:48 +0900

    テストを追加。

---
+++ test/test_regexp_hiragana.lua
@@ -0,0 +1,30 @@
+-- Copyright (C) 2017 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-parser.
+--
+-- dromozoa-parser is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-parser is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
+
+local builder = require "dromozoa.parser.builder"
+local regexp = require "dromozoa.parser.regexp"
+
+local P = builder.pattern
+local R = builder.range
+local S = builder.set
+
+local p
+  = P"あ" + P"い" + P"う" + P"え" + P"お"
+  + P"わ" + P"を" + P"ん"
+local a = regexp(p^"+"):nfa_to_dfa():minimize()
+a:write_graphviz("test.dot")
+
