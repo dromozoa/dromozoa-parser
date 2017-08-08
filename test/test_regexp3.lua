45dd7e38299fe031fcd366b3bc214a54d9ebc728
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-08-08 09:37:30 +0900

    テストを追加。

---
+++ test/test_regexp3.lua
@@ -0,0 +1,26 @@
+-- Copyright (C) 2017 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-parser.
+--
+-- dromozoa-parser is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-parser is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
+
+local builder = require "dromozoa.parser.builder"
+local regexp = require "dromozoa.parser.regexp"
+
+local RE = builder.regexp
+
+local _ = builder()
+
+local p = [[/*]] * (RE[[.*]] - RE[[.*\*\/.*]]) * [[*/]]
+regexp(p):nfa_to_dfa():minimize():write_graphviz("test-dfa.dot")
