8e8e51eff9a9f9c29862f5138be62a53b1f3d935
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-08-10 09:37:29 +0900

    テストを修正。

--- test/test_lua.lua
+++ test/test_lua.lua
@@ -15,17 +15,13 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
 
-local clone = require "dromozoa.commons.clone"
-local dumper = require "dromozoa.commons.dumper"
 local equal = require "dromozoa.commons.equal"
 local unix = require "dromozoa.unix"
 local builder = require "dromozoa.parser.builder"
+local driver = require "dromozoa.parser.driver"
 
 local timer = unix.timer()
 
-local P = builder.pattern
-local R = builder.range
-local S = builder.set
 local RE = builder.regexp
 local _ = builder()
 
@@ -132,11 +128,6 @@ _:search_lexer "long_comment"
   :when() :skip() :ret()
   :otherwise() :skip()
 
-timer:stop()
-print("define lexer", timer:elapsed())
-
-timer:start()
-
 _ :left "or"
   :left "and"
   :left "<" ">" "<=" ">=" "~=" "=="
@@ -149,11 +140,6 @@ _ :left "or"
   :left "*" "/" "//" "%"
   :right "not" "#" "UNM" "BNOT"
   :right "^"
-
-timer:stop()
-print("define percedence", timer:elapsed())
-
-timer:start()
 
 _"chunk"
   :_ "block"
@@ -346,18 +332,10 @@ _"fieldsep"
   :_ ","
   :_ ";"
 
-timer:stop()
-print("define grammar", timer:elapsed())
-
 timer:start()
 local lexer, grammar = _:build()
 timer:stop()
 print("build", timer:elapsed())
-
--- print("== symbols ==")
--- for i = 1, #grammar.symbol_names do
---   io.write("(", i, ") ", grammar.symbol_names[i], "\n")
--- end
 
 grammar:write_productions("test-productions.txt")
 
@@ -385,7 +363,31 @@ local parser, conflicts = grammar:lr1_construct_table(set_of_items, transitions)
 timer:stop()
 print("lr1_construct_table", timer:elapsed())
 
+lexer:compile("test_lexer.lua")
 parser:compile("test_parser.lua")
+
+do
+  local compiled_lexer = assert(loadfile("test_lexer.lua"))()()
+  collectgarbage()
+  collectgarbage()
+  local c1 = collectgarbage("count")
+  compiled_lexer = nil
+  collectgarbage()
+  collectgarbage()
+  local c2 = collectgarbage("count")
+  print("lexer memory", c1 - c2)
+end
+
+-- do
+--   local compiled_lexer = assert(loadfile("test_lexer.lua"))()()
+--   for i = 1, #lexer.lexers do
+--     lexer.lexers[i].items = nil
+--     lexer.lexers[i].name = nil
+--     lexer.lexers[i].type = nil
+--   end
+--   assert(equal(lexer, compiled_lexer))
+--   lexer = compiled_lexer
+-- end
 
 do
   local compiled_parser = assert(loadfile("test_parser.lua"))()()
@@ -409,220 +411,6 @@ grammar:write_set_of_items("test-set-of-items.txt", set_of_items)
 grammar:write_graphviz("test-graph.dot", set_of_items, transitions)
 grammar:write_table("test.html", parser)
 grammar:write_conflicts(io.stdout, conflicts)
-
---[====[
-do
-  local max_state = parser.max_state
-  local max_terminal_symbol = grammar.max_terminal_symbol
-  local max_symbol = parser.max_symbol
-  local parser_table = parser.table
-
-  do
-    local actions = {}
-    local gotos = {}
-    local map = {}
-
-    for state = 1, max_state do
-      for symbol = 1, max_symbol do
-        local i = state * max_symbol + symbol
-        local v = parser_table[i]
-        if v then
-          if symbol <= max_terminal_symbol then
-            local t = actions[state]
-            if not t then
-              t = {}
-              actions[state] = t
-            end
-            t[symbol] = v
-          else
-            local k = symbol - max_terminal_symbol
-            local t = gotos[state]
-            if not t then
-              t = {}
-              gotos[state] = t
-            end
-            t[k] = v
-          end
-          -- T[i] = v
-          -- local x = symbol
-          -- local y = state
-          -- local t = T[y]
-          -- if not t then
-          --   t = {}
-          --   T[y] = t
-          -- end
-          -- t[x] = v
-        end
-      end
-    end
-
-    local map = {}
-    for state = 1, max_state do
-      local a = actions[state]
-      if a then
-        local key = dumper.encode(a, {stable=true})
-        local b = map[key]
-        if map[key] then
-          print("hit action", key)
-          actions[state] = b
-        else
-          map[key] = a
-        end
-      end
-      local g = gotos[state]
-      if g then
-        local key = dumper.encode(a, {stable=true})
-        local h = map[key]
-        if not map[key] then
-          print("hit goto")
-          gotos[state] = h
-        else
-          map[key] = g
-        end
-      end
-    end
-
-    collectgarbage()
-    collectgarbage()
-    local c1 = collectgarbage("count")
-    actions = nil
-    gotos = nil
-    collectgarbage()
-    collectgarbage()
-    local c2 = collectgarbage("count")
-    print("count", c1 - c2)
-  end
-end
-]====]
-
-local function dump(file, value)
-  local out = assert(io.open(file, "w"))
-  out:write("return ")
-  out:write(dumper.encode(value, { pretty = true, stable = true }))
-  out:write("\n")
-  out:close()
-end
-
-dump("test_lexer_dump.lua", lexer.lexers)
-lexer:compile("test_lexer.lua")
-
-do
-  local compiled_lexer = assert(loadfile("test_lexer.lua"))()()
-  collectgarbage()
-  collectgarbage()
-  local c1 = collectgarbage("count")
-  compiled_lexer = nil
-  collectgarbage()
-  collectgarbage()
-  local c2 = collectgarbage("count")
-  print("lexer memory", c1 - c2)
-end
-
-local compiled_lexer = assert(loadfile("test_lexer.lua"))()()
-compiled_lexer:compile("test_lexer2.lua")
--- lexer = compiled_lexer
-
-local source_lexers = clone(lexer.lexers)
-for i = 1, #source_lexers do
-  local lexer = source_lexers[i]
-  lexer.items = nil
-  lexer.name = nil
-  lexer.type = nil
-end
-dump("test_source_lexer_dump.lua", source_lexers)
-dump("test_compiled_lexer_dump.lua", compiled_lexer.lexers)
-assert(equal(source_lexers, compiled_lexer.lexers))
-
---[====[
-do
-  local set_of_transitions = {}
-  do
-    local lexers = lexer.lexers
-    local TMAP = {}
-    for i = 1, #lexers do
-      local lexer = lexers[i]
-      local automaton = lexer.automaton
-      if automaton then
-        local max_state = automaton.max_state
-        local transitions = {}
---[[
-        for from = 1, max_state do
-          local empty = true
-          local copy = {}
-          for char = 0, 255 do
-            local to = automaton.transitions[char][from]
-            if to then
-              copy[char] = to
-              empty = false
-            end
-          end
-          if not empty then
-            local key = dumper.encode(copy)
-            if TMAP[key] then
-              print("hit!")
-              transitions[from] = TMAP[key]
-            else
-              TMAP[key] = copy
-              transitions[from] = copy
-            end
-          end
-        end
-]]
-
-        for char = 0, 255 do
-          local empty = true
-          local copy = {}
-          for from, to in pairs(automaton.transitions[char]) do
-            copy[from] = to
-            empty = false
-          end
-          if not empty then
-            local key = dumper.encode(copy)
-            if TMAP[key] then
-              -- print("hit!")
-              transitions[char] = TMAP[key]
-            else
-              TMAP[key] = copy
-              transitions[char] = copy
-            end
-          end
-        end
-
---[[
-        for char = 0, 255 do
-          local empty = true
-          for from, to in pairs(automaton.transitions[char]) do
-            local x = from
-            local y = char
-            -- local x = char
-            -- local y = from
-
-            local t = transitions[x]
-            if not t then
-              t = {}
-              transitions[x] = t
-            end
-            t[y] = to
-            empty = false
-          end
-        end
-]]
-        set_of_transitions[#set_of_transitions + 1] = transitions
-      end
-    end
-  end
-  collectgarbage()
-  collectgarbage()
-  local c1 = collectgarbage("count")
-
-  set_of_transitions = nil
-
-  collectgarbage()
-  collectgarbage()
-  local c2 = collectgarbage("count")
-  print("count", c1 - c2)
-end
-]====]
 
 local source = [====[
 f(a, b, c, d)
@@ -653,23 +441,10 @@ return {
 }
 ]====]
 
-parser.source = source
-
-local position = 1
-local root
-local node = {}
-repeat
-  local symbol, p, i, j, rs, ri, rj = assert(lexer(source, position))
-  root = assert(parser(symbol, p, i, j - 1, rs, ri, rj))
-  node.p = p
-  node.i = i
-  node.j = j
-  position = j
-until symbol == 1
+local terminal_nodes = assert(lexer(source))
+local root = assert(parser(terminal_nodes, source))
 
 parser:write_graphviz("test.dot", root)
-
-print(root[0], root.p)
 
 local result = {}
 
@@ -697,6 +472,7 @@ while true do
     stack2[n2 + 1] = node
   end
 end
+local node = terminal_nodes[#terminal_nodes]
 result[#result + 1] = source:sub(node.p, node.i - 1)
 
 io.write(table.concat(result))
