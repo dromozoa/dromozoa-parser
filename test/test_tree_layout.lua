ff6cd6a4879cf07bface6d78f2d7fb901f34eb41
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-08-30 13:18:56 +0900

    木構造のレイアウトを追加。

--- test/test_tree_layout.lua
+++ test/test_tree_layout.lua
@@ -15,34 +15,31 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
 
-local function f(a, b, c, ...)
-  local x = { a = { b = { c = a } } }
-  print(a, b and 1 or 2, c .. c, x.a.b.c)
-  return 42
-end
-f("foo", true, [[
-abc
-def
-ghi
-]])
+local tree_layout = require "dromozoa.parser.tree_layout"
 
---12345678901234567890123456789
-data = { 1, 2, 3, 4, "日本語" }
-repeat
-  local n = #data
-  print(data[n])
-  data[n] = nil
-until n == 1
-
-do
-  local a, b = 1, 2
-  local b = 3
-  print(a, b)
+local nodes = {}
+for i = 1, 23 do
+  nodes[i] = { name = tostring(i) }
 end
 
-local i = 10
-for i = 1, i do
-  print(i)
+local links = {
+  [1] = { 2, 13 };
+  [2] = { 3, 12 };
+  [3] = { 4, 11 };
+  [4] = { 5, 6 };
+  [6] = { 7, 8 };
+  [8] = { 9, 10 };
+  [13] = { 14, 15 };
+  [15] = { 16, 17 };
+  [17] = { 18, 19 };
+  [18] = { 20, 21 };
+  [20] = { 22, 23 };
+}
+
+for id, link in pairs(links) do
+  local node = nodes[id]
+  node[1] = nodes[link[1]]
+  node[2] = nodes[link[2]]
 end
 
---[[ end ]]
+tree_layout(nodes[1])
