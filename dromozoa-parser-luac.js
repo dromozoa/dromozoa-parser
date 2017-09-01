a65d24a0751b4755b5e783cf51d4ad0fb3f6c4a8
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-09-01 09:18:39 +0900

    Luaコンパイラを修正。

--- dromozoa-parser-luac.js
+++ dromozoa-parser-luac.js
@@ -2,13 +2,15 @@
 "use strict";
 (function (root) {
   var console = root.console;
+  var parseFloat = root.parseFloat;
   var $ = root.jQuery;
   var d3 = root.d3;
   var setting = root.dromozoa.parser.setting;
 
-  $(function () {
-    var svg = d3.select(".tree > svg");
+  $.noop(console);
 
+  $(function () {
+    var svg = d3.select(".tree svg");
     var zoom = d3.zoom();
     var zoom_x;
     var zoom_y;
@@ -23,5 +25,32 @@
         svg.select(".view").attr("transform", transform);
       }))
       .call(zoom.transform, d3.zoomIdentity.translate(setting.node_width * 0.5, setting.tree_height * 0.5));
+
+    $(".S").on("click", function () {
+      var $T = $(this);
+      var id = $T.attr("id").substr(1);
+      var $S = $("#T" + id);
+      var t = $S.attr("transform").match(/^translate\((.*?),(.*?)\)$/);
+      var zx = setting.tree_width * 0.5 - parseFloat(t[1]) * zoom_k;
+      var zy = setting.tree_height * 0.5 - parseFloat(t[2]) * zoom_k;
+
+      $(".active").removeClass("active");
+      $S.addClass("active");
+      $T.addClass("active");
+      svg.select(".viewport")
+        .transition().duration(400)
+        .call(zoom.transform, d3.zoomIdentity.translate(zx, zy).scale(zoom_k));
+    });
+
+    $(".T").on("click", function () {
+      var $T = $(this);
+      var id = $T.attr("id").substr(1);
+      var $S = $(".S" + id);
+
+      $(".active").removeClass("active");
+      $S.addClass("active");
+      $T.addClass("active");
+      $("body").animate({ scrollTop: $S.offset().top });
+    });
   });
 }(this.self));
