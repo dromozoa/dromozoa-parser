98b3dc38456ced55c49a5a1c5cd686b4d42c4e59
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-09-01 11:41:05 +0900

    Lua 5.3のパーサを修正。

--- generate_lua53.lua
+++ generate_lua53.lua
@@ -96,30 +96,30 @@ _:lexer()
   :_ ".."
   :_ "..."
   :_ (RE[[[A-Za-z_]\w*]]) :as "Name"
-  :_ (RE[["[^\\"]*"]]) :as "LiteralString" :sub(2, -2) :attr("type", "string")
-  :_ (RE[['[^\\']*']]) :as "LiteralString" :sub(2, -2) :attr("type", "string")
-  :_ ("[[\n" * (RE[[.*]] - RE[[.*\]\].*]]) * "]]") :as "LiteralString" :sub(4, -3) :attr("type", "string")
-  :_ ("[[" * (RE[[.*]] - RE[[.*\]\].*]]) * "]]") :as "LiteralString" :sub(3, -3) :attr("type", "string")
+  :_ (RE[["[^\\"]*"]]) :as "LiteralString" :sub(2, -2)
+  :_ (RE[['[^\\']*']]) :as "LiteralString" :sub(2, -2)
+  :_ ("[[\n" * (RE[[.*]] - RE[[.*\]\].*]]) * "]]") :as "LiteralString" :sub(4, -3)
+  :_ ("[[" * (RE[[.*]] - RE[[.*\]\].*]]) * "]]") :as "LiteralString" :sub(3, -3)
   :_ [["]] :skip() :call "dq_string" :mark()
   :_ [[']] :skip() :call "sq_string" :mark()
   :_ (RE[[\[=*\[\n]]) :sub(2, -3) :join("]", "]") :hold() :skip() :call "long_string" :mark()
   :_ (RE[[\[=*\[]]) :sub(2, -2) :join("]", "]") :hold() :skip() :call "long_string" :mark()
-  :_ (RE[[\d+]]) :as "IntegerConstant" :attr("type", "integer")
-  :_ (RE[[0[xX][0-9A-Fa-f]+]]) :as "IntegerConstant" :attr("type", "integer")
-  :_ (RE[[(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?]]) :as "FloatConstant" :attr("type", "float")
-  :_ (RE[[0[xX]([0-9A-Fa-f]+(\.[0-9A-Fa-f]*)?|\.[0-9A-Fa-f]+)([pP][+-]?\d+)?]]) :as "FloatConstant" :attr("type", "float")
+  :_ (RE[[\d+]]) :as "IntegerConstant"
+  :_ (RE[[0[xX][0-9A-Fa-f]+]]) :as "IntegerConstant"
+  :_ (RE[[(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?]]) :as "FloatConstant"
+  :_ (RE[[0[xX]([0-9A-Fa-f]+(\.[0-9A-Fa-f]*)?|\.[0-9A-Fa-f]+)([pP][+-]?\d+)?]]) :as "FloatConstant"
   :_ ("--[[" * (RE[[.*]] - RE[[.*\]\].*]]) * "]]") :skip()
   :_ (RE[[--\[=+\[]]) :sub(4, -2) :join("]", "]") :hold() :skip() :call "long_comment"
   :_ ("--" * (RE[[[^\n]*]] - RE[[\[=*\[.*]]) * "\n") : skip()
 
 string_lexer(_:lexer "dq_string")
-  :_ [["]] :as "LiteralString" :concat() :ret() :attr("type", "string")
+  :_ [["]] :as "LiteralString" :concat() :ret()
 
 string_lexer(_:lexer "sq_string")
-  :_ [[']] :as "LiteralString" :concat() :ret() :attr("type", "string")
+  :_ [[']] :as "LiteralString" :concat() :ret()
 
 _:search_lexer "long_string"
-  :when() :as "LiteralString" :concat() :ret() :attr("type", "string")
+  :when() :as "LiteralString" :concat() :ret()
   :otherwise() :push()
 
 _:search_lexer "long_comment"
@@ -140,7 +140,7 @@ _ :left "or"
   :right "^"
 
 _"chunk"
-  :_ "block" :attr "state" :attr "scope"
+  :_ "block"
 
 _"block"
   :_ ()
@@ -154,22 +154,22 @@ _"stats"
 
 _"stat"
   :_ ";"
-  :_ "varlist" "=" "explist" :attr("order", {3,2,1})
+  :_ "varlist" "=" "explist" {3,2,1}
   :_ "functioncall"
   :_ "label"
   :_ "break"
   :_ "goto" "Name"
-  :_ "do" "block" "end" :attr "scope"
-  :_ "while" "exp" "do" "block" "end" :attr "scope" :attr "loop"
-  :_ "repeat" "block" "until" "exp" :attr "scope" :attr "loop"
+  :_ "do" "block" "end"
+  :_ "while" "exp" "do" "block" "end"
+  :_ "repeat" "block" "until" "exp"
   :_ "if_clauses" "end"
-  :_ "for" "local_name" "=" "exp" "," "exp" "do" "block" "end" :attr("order", {1,4,5,6,3,2,7,8,9}) :attr "scope" :attr "loop"
-  :_ "for" "local_name" "=" "exp" "," "exp" "," "exp" "do" "block" "end" :attr("order", {1,4,5,6,7,8,3,2,9,10,11}) :attr "scope" :attr "loop"
-  :_ "for" "namelist" "in" "explist" "do" "block" "end" :attr("order", {1,4,3,2,5,6,7}) :attr "scope" :attr "loop"
+  :_ "for" "local_name" "=" "exp" "," "exp" "do" "block" "end" {1,4,5,6,3,2,7,8,9}
+  :_ "for" "local_name" "=" "exp" "," "exp" "," "exp" "do" "block" "end" {1,4,5,6,7,8,3,2,9,10,11}
+  :_ "for" "namelist" "in" "explist" "do" "block" "end" {1,4,3,2,5,6,7}
   :_ "function" "funcname" "funcbody"
   :_ "local" "function" "local_name" "funcbody"
   :_ "local" "namelist"
-  :_ "local" "namelist" "=" "explist" :attr("order", {1,4,3,2})
+  :_ "local" "namelist" "=" "explist" {1,4,3,2}
 
 _"if_clauses"
   :_"if_clause"
@@ -182,13 +182,13 @@ _"elseif_clauses"
   :_ "elseif_clause" "elseif_clauses"
 
 _"if_clause"
-  :_ "if" "exp" "then" "block" :attr "scope"
+  :_ "if" "exp" "then" "block"
 
 _"elseif_clause"
-  :_ "elseif" "exp" "then" "block" :attr "scope"
+  :_ "elseif" "exp" "then" "block"
 
 _"else_clause"
-  :_ "else" "block" :attr "scope"
+  :_ "else" "block"
 
 _"retstat"
   :_ "return"
@@ -220,19 +220,19 @@ _"var"
 
 _"namelist"
   :_ "Name"
-  :_ "namelist" "," "Name" {[1]={2,3}}
+  :_ "namelist" "," "Name" {[1]={3}}
 
 _"explist"
   :_ "exp"
-  :_ "explist" "," "exp" {[1]={2,3}}
+  :_ "explist" "," "exp" {[1]={3}}
 
 _"exp"
   :_ "nil"
   :_ "false"
   :_ "true"
-  :_ "IntegerConstant" :attr "loadk"
-  :_ "FloatConstant" :attr "loadk"
-  :_ "LiteralString" :attr "loadk"
+  :_ "IntegerConstant"
+  :_ "FloatConstant"
+  :_ "LiteralString"
   :_ "..."
   :_ "functiondef"
   :_ "prefixexp"
@@ -261,10 +261,10 @@ _"exp"
   :_ "exp" "and" "exp"
   :_ "exp" "or" "exp"
   -- unop
-  :_ "-" "exp" :prec "UNM" :attr "unop"
-  :_ "not" "exp" :attr "unop"
-  :_ "#" "exp" :attr "unop"
-  :_ "~" "exp" :prec "BNOT" :attr "unop"
+  :_ "-" "exp" :prec "UNM"
+  :_ "not" "exp"
+  :_ "#" "exp"
+  :_ "~" "exp" :prec "BNOT"
 
 -- prefixexp without functioncall
 _"prefixexp"
@@ -287,8 +287,8 @@ _"functiondef"
   :_ "function" "funcbody"
 
 _"funcbody"
-  :_ "(" ")" "block" "end" :attr "state" :attr "scope" :attr(4, "funcbody_end") {3}
-  :_ "(" "parlist" ")" "block" "end" :attr "state" :attr "scope" :attr(5, "funcbody_end") {4,2}
+  :_ "(" ")" "block" "end" :attr(4, "funcbody_end") {3}
+  :_ "(" "parlist" ")" "block" "end" :attr(5, "funcbody_end") {4,2}
 
 _"parlist"
   :_ "namelist"
@@ -302,7 +302,7 @@ _"tableconstructor"
 
 _"fieldlist"
   :_ "field"
-  :_ "fieldlist" "fieldsep" "field" {[1]={2,3}}
+  :_ "fieldlist" "fieldsep" "field" {[1]={3}}
 
 _"field"
   :_ "[" "exp" "]" "=" "exp"
