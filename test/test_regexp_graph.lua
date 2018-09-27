2471885947eacad763523398de930e18feb31ea8
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-09-27 20:51:19 +0900

    オートマトンのSVG出力を調整。

--- test/test_regexp_graph.lua
+++ test/test_regexp_graph.lua
@@ -19,9 +19,6 @@ local builder = require "dromozoa.parser.builder"
 local regexp = require "dromozoa.parser.regexp"
 local to_graph = require "dromozoa.parser.regexp.to_graph"
 
-local layout = require "dromozoa.graph.layout"
-local render = require "dromozoa.graph.render"
-local subdivide_special_edges = require "dromozoa.graph.subdivide_special_edges"
 local vecmath = require "dromozoa.vecmath"
 
 local dom = require "dromozoa.dom"
@@ -38,33 +35,41 @@ local p = P"/*" * (P(1)^"*" - P(1)^"*" * P"*/" * P(1)^"*") * P"*/"
 local nfa = regexp(p)
 local dfa = nfa:nfa_to_dfa():minimize()
 
+local start_state = dfa.start_state
+print(start_state)
+
 local g, e_labels = to_graph(dfa)
-e_labels = e_labels or {}
-local last_uid = g.u.last
-local last_eid = g.e.last
-local revered_eids = subdivide_special_edges(g, e_labels)
-local x, y, paths = layout(g, last_uid, last_eid, revered_eids)
-
-local matrix = vecmath.matrix3(0, 100, 50, 100, 0, 50, 0, 0, 1)
-local node = render(g, last_uid, last_eid, x, y, paths, {
-  matrix = matrix;
+local root = g:render {
+  matrix = vecmath.matrix3(0, 100, 50, 50, 0, 25, 0, 0, 1);
   e_labels = e_labels;
-  max_text_length = 72;
-  curve_parameter = 1;
-})
+}
 
-local size = matrix:transform(vecmath.vector2(x.max + 1, y.max + 1))
+local u_paths = root[1]
+local u_texts = root[2]
+local e_paths = root[3]
+local e_texts = root[4]
+
+for i = 1, #u_paths do
+  local path = u_paths[i]
+  local uid = path["data-uid"]
+  if uid == start_state then
+    local text = u_texts[i]
+    path.class = "start_state"
+    text.class = "start_state"
+  end
+end
+
+local e_bg_texts = _"g" { class ="e_bg_texts" }
+for i = 1, #e_texts do
+  e_bg_texts[i] = e_texts[i]
+end
 
 local doc = dom.xml_document(_"svg" {
   version = "1.1";
   xmlns = "http://www.w3.org/2000/svg";
-  width = size.x;
-  height = size.y;
+  width = root["data-width"];
+  height = root["data-height"];
   _"defs" {
-    _"style" {
-      type = "text/css";
-      "@import url('https://dromozoa.github.io/dromozoa-graph/sample.css');";
-    };
     _"marker" {
       id = "arrow";
       viewBox = "0 0 4 4";
@@ -74,12 +79,19 @@ local doc = dom.xml_document(_"svg" {
       markerHeight = 8;
       orient = "auto";
       _"path" {
-        d = svg.path_data():M(0,0):L(0,4):L(4,2):Z();
+        d = svg.path_data():M(0,0):L(4,2):L(0,4):Z();
       };
     };
   };
-  node;
+  u_paths;
+  u_texts;
+  e_paths;
+  e_bg_texts;
+  e_texts;
 })
+doc.stylesheets = {
+  { href = "docs/automaton.css" }
+}
 
 local out = assert(io.open("test.svg", "w"))
 doc:serialize(out)
