4aa45986102ea138f3bb2513a043d5296e2635ff
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-10-02 23:04:26 +0900

    テストを修正。

--- test/test_search_lexer.lua
+++ test/test_search_lexer.lua
@@ -15,14 +15,10 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
 
-local equal = require "dromozoa.commons.equal"
 local builder = require "dromozoa.parser.builder"
 local symbol_value = require "dromozoa.parser.symbol_value"
 
-local P = builder.pattern
-local R = builder.range
 local S = builder.set
-
 local _ = builder()
 
 _:lexer()
@@ -45,13 +41,11 @@ foo [===[ bar ]===] baz
 local result = assert(lexer(source))
 assert(symbol_value(result[1]) == "test")
 assert(symbol_value(result[2]) == "\nfoo [===[ bar ]===] baz\n")
-assert(result[3][0] == 1)
+assert(result[3][0] == 1) -- $
 
 local data = {}
 for i = 1, #result do
   local item = result[i]
   data[#data + 1] = source:sub(item.p, item.j)
 end
-print(#source, result[#result].p, result[#result].i, result[#result].j, ("%q"):format(data[#data]))
-io.write(table.concat(data))
 assert(table.concat(data) == source)
