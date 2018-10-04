1f225b6ad4b76033c61f966a8c4cdfe9e8d2d5f7
Tomoyuki Fujimori <moyu@dromozoa.com>
2018-10-05 08:52:50 +0900

    ランタイム生成を追加。

--- dromozoa/parser/parser.lua
+++ dromozoa/parser/parser.lua
@@ -15,15 +15,8 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-parser.  If not, see <http://www.gnu.org/licenses/>.
 
-local dump = require "dromozoa.parser.dump"
+local compile = require "dromozoa.parser.parser.compile"
 local execute = require "dromozoa.parser.parser.execute"
-
-local function compile(self, out)
-  out:write("local parser = require \"dromozoa.parser.parser\"\n")
-  local root = dump(out, self)
-  out:write("return function () return parser(", root, ") end\n")
-  return out
-end
 
 local class = {}
 local metatable = {
